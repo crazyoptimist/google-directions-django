b12f7e910c562cd21d0f33c0294018b806912d54
crazyoptimist <hey@crazyoptimist.net>
2023-02-17 11:28:26 -0600

    chore: add app to project settings

--- toohla_project/settings.py
+++ toohla_project/settings.py
@@ -53,6 +53,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'carbon_emissions',
+
+    # Third party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
